42dbf41893f5be0b3327982d98c5e019f67d2085
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-03-12 18:12:21 +0100

    rpn done

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 22:35:08 by plichota          #+#    #+#             */
-/*   Updated: 2026/03/12 17:30:09 by plichota         ###   ########.fr       */
+/*   Updated: 2026/03/12 18:11:58 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,44 +45,9 @@ static int isOperator(char c)
     return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
-/*
-    LAMBDA EXPRESSION (C++11)
-    function that takes parameters and returns a value, without having to name the function
-    [capture](parameters) -> return_type { body }
-
-    es.
-    instead of writing isValid we can write
-    auto isValid = [](char c) { return std::isdigit(c) || c == '+' || c == '-' || c == '*' || c == '/' || std::isspace(c); };
-*/
-
-/*
-    CHECK IF ALL ELEMENTS SATISFY A CONDITION (C++11)
-    std::all_of(s.begin(), s.end(), foo)
-*/
-
-// int RPN::parse_and_fill_stack(std::string s)
-// {
-//     // has to contain only digits or operators or spaces
-//     for (std::string::iterator it = s.begin(); it != s.end(); ++it)
-//     {
-//         if (!isValid(*it)) {
-//             std::cerr << "Error" << std::endl;
-//             return 1;
-//         }
-//     }
-
-//     // insert characters in stack IN REVERSE (LIFO)
-//     for (size_t i = s.size(); i > 0; i--)
-//     {
-//         if (s[i - 1] != ' ')
-//             stack.push(s[i - 1]);
-//     }
-//     return 0;
-// }
-
 static int apply_operator(int a, int b, char op)
 {
-    std::cout << YELLOW << "apply operator: " << a << " " << op << " " << b << RESET << std::endl;
+    // std::cout << YELLOW << "apply operator: " << a << " " << op << " " << b << RESET << std::endl;
     if (op == '+')
         return a + b;
     else if (op == '-')
@@ -94,70 +59,11 @@ static int apply_operator(int a, int b, char op)
     return INT_MAX; // should never happen
 }
 
-// void RPN::compute()
-// {
-//     bool first = true;
-//     int result = 0;
-
-//     while (stack.size() >= 2) // non ripusho il result, tengo come int a parte
-//     {
-//         if (first)
-//         {
-//             std::cout << "is first, so pop : " << stack.top() << std::endl;
-//             char c1 = stack.top(); // prendo elemento
-//             stack.pop(); // elimino elemento
-//             if (!std::isdigit(c1))
-//             {
-//                 std::cerr << "Error: Not a number" << std::endl;
-//                 return ;
-//             }
-//             result = c1 - '0';
-//             first = false;
-//         }
-
-//         std::cout << "pop second number : " << stack.top() << std::endl;
-//         char c2 = stack.top(); // prendo elemento
-//         stack.pop(); // elimino elemento
-//         if (!std::isdigit(c2))
-//         {
-//             std::cerr << "Error: Not enough operands" << std::endl;
-//             return ;
-//         }
-//         int n2 = c2 - '0';
-
-//         std::cout << "pop operator : " << stack.top() << std::endl;
-//         char op = stack.top(); // prendo operatore
-//         stack.pop(); // elimino elemento
-//         if (!isOperator(op))
-//         {
-//             std::cerr << "Error: Not an operator" << std::endl;
-//             return ;
-//         }
-//         // apply operator
-//         std::cout << "apply operator " << op << " on " << result << " and " << n2 << std::endl;
-//         result = apply_operator(result, n2, op);
-//         std::cout << "result : " << result << std::endl;
-//         if (result == INT_MAX)
-//         {
-//             std::cerr << "Error: Invalid operator" << std::endl;
-//             return ;
-//         }
-//     }
-//     if (stack.size() != 0)
-//     {
-//         std::cerr << "Error" << std::endl;
-//         std::cout << "Remaining elements in stack : " << std::endl;
-//         print_LIFO();
-//     }
-//     std::cout << result << std::endl;
-// }
-
 static int isValidString(const std::string& s)
 {
     for (std::string::const_iterator it = s.begin(); it != s.end(); ++it)
     {
         if (!isValid(*it)) {
-            std::cerr << "Error" << std::endl;
             return 0;
         }
     }
@@ -206,8 +112,8 @@ void RPN::compute(std::string s)
     if (temp.size() != 1)
     {
         std::cerr << "Error" << std::endl;
-        std::cout << "Remaining elements in stack : " << std::endl;
-        print_LIFO();
+        // std::cout << "Remaining elements in stack : " << std::endl;
+        // print_LIFO();
         return ;
     }
     std::cout << temp.top() << std::endl;
